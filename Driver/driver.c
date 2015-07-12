995dc5e1d2344d3a51979eb5fa084ff800d20190
njmaysonet <audacious.anarchist@gmail.com>
2015-07-12 03:07:39 -0400

    added command directives

--- Driver/driver.c
+++ Driver/driver.c
@@ -1,14 +1,65 @@
 #include "scanner.h"
 #include "parserwithcodegen.h"
 #include "pmachine.h"
+#include "string.h"
 
-int main()
+int main(int argc, char** argv)
 {
-  scannermain();
+  int i;
+  int lFlag;
+  int aFlag;
+  int vFlag;
+  
+  //Loops through arguments, argv[0] is file name, file arguments
+  //start at argv[1], will loop through argv[1] to argv[argc-1]
+  //and set flags based on commands, if the directives are incorrectly formatted
+  //they are ignored
+  for(i = 1; i < argc; i++)
+  {
+    if(strcmp(argv[i],"-l") == 0)
+    {
+      lFlag = 1;
+    }
+    else if(strcmp(argv[i], "-a") == 0)
+    {
+      aFlag = 1;
+    }
+    else if(strcmp(argv[i], "-v") == 0)
+    {
+      vFlag = 1;
+    }
+  }
+  char buffer[3][10]
+  strcpy(buffer[1],"print");
+  
+  if(lFlag == 1)
+  {
+    scannermain(2,buffer)
+  }
+  else
+  {
+    scannermain(0, buffer);
+  }
   printf("Scanner successful\n");
-  parsermain();
+  
+  if(aFlag == 1)
+  {
+    parsermain(2,buffer);
+  }
+  else
+  {
+    parsermain(0,buffer);
+  }
   printf("Parsing and Code Generating successful\n");
-  pmachinemain();
+  
+  if(vFlag == 1)
+  {
+    pmachine(2, buffer);
+  }
+  else
+  {
+    pmachinemain(0,buffer);
+  }
   printf("Everything done\n");
   return 0;
 }
